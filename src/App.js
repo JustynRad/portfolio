1b30d31f39fc7f8d8db15e991662cad0cb1d20ec
Justyn Radovan <jradovan@ucsd.edu>
2021-12-22 02:09:59 -0800

    created responsive navbar using react and styled component react libraries

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import NavBar from './components/NavBar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import Photography from './pages/Photography';
+import Contact from './pages/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route exact path ="/" element={<Home/>}/>
+          <Route exact path ="/about" element={<About/>}/>
+          <Route exact path ="/projects" element={<Projects/>}/>
+          <Route exact path ="/photography" element={<Photography/>}/>
+          <Route exact path ="/contact" element={<Contact/>}/>
+        </Routes>
+      </Router>
+    </>
+  )
 }
 
 export default App;
+
